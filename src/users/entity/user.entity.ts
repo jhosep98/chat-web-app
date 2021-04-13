00a98b30fa1e2976bca61a1ba01c43aa2a930ca8
jhosep98 <jhosep149@hotmail.com>
2021-04-13 13:09:34 -0300

    Create entity for users

---
+++ src/users/entity/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: false, select: false })
+  password: string;
+
+  @Column({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+}
