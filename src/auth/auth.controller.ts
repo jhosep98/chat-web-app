d36bb77f859e9c889529dd56714727dcc757e3e3
jhosep98 <jhosep149@hotmail.com>
2021-04-14 12:31:36 -0300

    create custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { User } from 'src/decorators';
+import { UserEntity } from 'src/users/entity/user.entity';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Req() req: Request) {
-    return req.user;
+  login(@User() user: UserEntity) {
+    return user;
   }
 
   @Get('profile')
